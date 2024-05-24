2c4a0582913511410417f8867d47f1d5fd7da652
Manish <7055ranamanish@gmail.com>
2024-05-24 23:03:31 +0530

    frontend setup almost complete

---
+++ FrontEnd/src/Scripts/reviews.js
@@ -0,0 +1,52 @@
+export const customerReviews = [
+    {
+      "name": "Rahul",
+      "location": "Kolkata, India",
+      "message": "Docs&Drugs transformed my healthcare experience! The online consultations were so convenient, and the doctors were knowledgeable and caring."
+    },
+    {
+      "name": "Pratham",
+      "location": "UP, India",
+      "message": "I found the perfect specialist for my condition through Docs&Drugs. The personalized treatment plan made all the difference. Thank you for prioritizing my health!"
+    },
+    {
+      "name": "Ayush",
+      "location": "HP, India",
+      "message": "Booking appointments was a breeze, and the service exceeded my expectations. The doctors truly listen and provide effective solutions. 5 stars!"
+    },
+    {
+      "name": "Vaibhav",
+      "location": "Rajasthan, India",
+      "message": "Docs&Drugs is a game-changer! The emergency care saved me during a critical situation. Grateful for their quick and efficient support."
+    },
+    {
+      "name": "Keshav",
+      "location": "Bihar, India",
+      "message": "I used to dread dental visits, but Docs&Drugs made it a pleasant experience. The dentist was gentle and professional. I'll definitely be back!"
+    },
+    {
+      "name": "Nachiket",
+      "location": "Maharashtra, India",
+      "message": "Finally, a healthcare platform that puts patients first! The heart disease service provided comprehensive care and regular follow-ups. Thank you, Docs&Drugs!"
+    },
+    {
+      "name": "Aditya",
+      "location": "UP, India",
+      "message": "I've been using Docs&Drugs for a variety of health concerns, and each time, I received exceptional care. It's my go-to for all medical needs!"
+    },
+    {
+      "name": "Lakshya",
+      "location": "UP, India",
+      "message": "I highly recommend Docs&Drugs for online consultations. It's convenient, secure, and the doctors are top-notch. Great job, team!"
+    },
+    {
+      "name": "Deepika",
+      "location": "Maharashtra, India",
+      "message": "The convenience of accessing medical notes online was fantastic. Docs&Drugs made managing my health records hassle-free! Best Healthcare services."
+    },
+    {
+      "name": "Akshita",
+      "location": "HP, India",
+      "message": "As a busy professional, Docs&Drugs has been a lifesaver. Quick prescriptions and refills without compromising on quality care. Thank you!"
+    }
+];
